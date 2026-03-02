122f22ec5c8c5e28a002ed4dc3c1f7f24d34c107
Yuvraj Patil <5191208+night-fury-rider@users.noreply.github.com>
2026-03-02 16:31:42 +0530

    Fixed base path issue of deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import path from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/utilities/password-generator/",
   plugins: [react()],
   resolve: {
     alias: {
